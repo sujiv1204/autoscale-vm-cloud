8d7f109bacd4006c27f3dec305b68e06dee71ffe
sujiv <sujivraj1204@gmail.com>
2026-03-28 02:08:56 +0530

    fix: Increase CPU stress intensity to 8 for 2-CPU VM

--- trigger_load.sh
+++ trigger_load.sh
@@ -3,6 +3,6 @@
 echo "Generating CPU load..."
 curl -X POST http://localhost:5000/stress/cpu \
   -H "Content-Type: application/json" \
-  -d '{"duration": 300, "intensity": 2}'
+  -d '{"duration": 300, "intensity": 8}'
 echo ""
 echo "Load triggered! Monitor autoscaler output."
